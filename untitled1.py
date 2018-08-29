f9354e1fac151f318afc1c01486b206219b771ab
SH2OCN <40562788+SH2OCN@users.noreply.github.com>
2018-08-29 22:14:49 +0800

    Upppdate...

--- untitled1.py
+++ untitled1.py
@@ -8,6 +8,7 @@ Created on Wed Aug 29 15:33:43 2018
 import bisect
 t = [0,1,2,3,4]
 x = 1.0
-print(bisect.bisect(t,x))
-bisect.insort(t,x)
+print(bisect.bisect_right(t,x))
+bisect.insort_left(t,x)
 print(t)
+bisect.
