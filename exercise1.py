2ac26509b924c0f0aa6c8fb31314d49253fc27f7
SH2OCN <40562788+SH2OCN@users.noreply.github.com>
2018-08-31 17:05:47 +0800

    0831

--- exercise1.py
+++ exercise1.py
@@ -1,36 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from bisect import bisect_left
+t = [[0 for j in range(3)] for i in range(3)]
+print(t)
+t[0][1]=2
+print(t)
+print('-------'*8)
 
 
-def make_word_list():
-    """Reads lines from a file and builds a list using append."""
-    word_list = []
-    fin = open('words.txt')
-    for line in fin:
-        word = line.strip()
-        word_list.append(word)
-    return word_list
+import numpy
+c = numpy.zeros((3,3),int)
+print(c)
+c[0][1] = 2
+print(c)
+print('-------'*8)
 
-
-def in_bisect(word_list, word):
-    """Checks whether a word is in a list using bisection search.
-
-    Precondition: the words in the list are sorted
-
-    word_list: list of strings
-    word: string
-    """
-    i = bisect_left(word_list, word)
-    if i != len(word_list) and word_list[i] == word:
-        return True
-    else:
-        return False
-
-
-if __name__ == '__main__':
-    word_list = make_word_list()
-    
-    for word in ['alien', 'allen']:
-        print word, 'in list', in_bisect(word_list, word)
-
+#a = [[0]*3]*3
+a = [[0,0,0] for i in range(4)]
+print(a)
+a[0][1]=2
+print(a)
